dfef42cbec9bcd77b8a167814a2fcb1d78cd61c1
Dalton Lim <39323194+daltonlim@users.noreply.github.com>
2019-05-31 22:47:41 +1200

    finally

--- checksum.py
+++ checksum.py
@@ -7,4 +7,4 @@ def internet_checksum(data, total=0x0):
     1. https://tools.ietf.org/html/rfc1071
     2. http://www.netfor2.com/checksum.html
     '''
-	pass # TODO: Implement this function and return the checksum
+	pass # TODO: Implement this function and return the internet checksum
